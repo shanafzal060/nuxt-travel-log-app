f1ed096c770de63aa5eda0a82a05a0df570845cb
shanafzal060 <eng.zeeshanafzal@gmail.com>
2025-09-03 20:10:55 +0500

    show avatar after login and setup log out functionalty

--- app/components/signin-button.vue
+++ app/components/signin-button.vue
@@ -6,7 +6,25 @@ const store = useAuthStore();
 
 <template>
   <div>
-    <button :disabled="store.loading" class="btn btn-accent" @click="store.signIn">
+    <div v-if="store.user && !store.loading" class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class=" mr-4">
+        <div v-if="store.user.image" class="avatar avatar-online">
+          <div class="w-8 rounded-full">
+            <img :src="store.user.image" :alt="store.user.name">
+          </div>
+        </div>
+        <div v-else class="btn">
+          {{ store.user.name }}
+        </div>
+      </div>
+      <ul tabindex="0" class="dropdown-content menu bg-accent rounded-box z-1 w-52 p-2 shadow-sm">
+        <li>
+          <a @click="store.signOut">
+            <icon name="tabler:logout-2" size="20" />Sign Out</a>
+        </li>
+      </ul>
+    </div>
+    <button v-else :disabled="store.loading" class="btn btn-accent" @click="store.signIn">
       Sign In with Github
       <span v-if="store.loading" class="loading loading-spinner" />
       <icon v-else name="tabler:brand-github" size="20" />
